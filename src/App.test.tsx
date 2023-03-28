e2d48b533b331efaa13bfcd84815b4f6e57e68c0
Hoang Loc <hoanggdloc@gmail.com>
2023-03-29 00:56:13 +0700

    [IMPORTANT]: Setup Project Vite, Eslint, vitest, prettier, react-router-dom...

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import App from './App';
+
+describe('App', () => {
+  it('render hello world', () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello World');
+  });
+});
